f5708f2a39d59dff60cce131e6936b2655aa5605
rzl <rzl@rzl-Lenovo-3000-G530.(none)>
2014-02-28 12:20:46 +0545

    add Comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to post_path(@post)
+  end
+
+  def read
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+  
 end
